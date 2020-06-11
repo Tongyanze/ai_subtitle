8b3726bd9a79c47e4e117c23e97b69335740a9a6
tyz <tyzjslyg@126.com>
2020-06-11 21:43:47 +0800

    新建文件夹

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="header">
+        <div class="header-logo">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+    .header{
+        position: fixed;
+        z-index: 1000;
+        height: 60px;
+        right: 0;
+        left: 0;
+        top: 0;
+        background: rgb(107, 154, 255);
+        box-shadow: 0 1px 10px -6px rgba(0,0,0,0.42), 0 1px 10px 0 rgba(0,0,0,0.12), 0 4px 5px -2px rgba(0,0,0,0.1);
+    }
+
+    .header-logo{
+        
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
+
   <div class="home">
+    <Header/>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -8,10 +10,11 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import Header from "@/components/Header.vue";
 export default {
   name: 'Home',
   components: {
+    Header,
     HelloWorld
   }
 }
