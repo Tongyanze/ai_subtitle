69ffd86c9e675a95be1e7bb7ee9658c1c28de2a8
Halfmooner <1056356517@qq.com>
2020-06-28 14:53:44 +0800

    似乎有没更新的或者被顶掉的

--- src/views/userinfo/changeinfo.vue
+++ src/views/userinfo/changeinfo.vue
@@ -1,14 +1,17 @@
 <template>
       <div  class="changebasic">
-                           <div  class="changesubInfo">
+                           
+                           
+                        
+                            <div  class="changesubInfo">
+                               
                                 昵称： <input type="text" id="changename"  class="change" v-model="username"/>
                             </div>
                                <div  class="changesubInfo">
                                 签名： <textarea type="text" id="changesign" class="change" v-model="usersign"/>
                             </div>
                             <div  class="changesubInfo">
-                                性别： <button class="selectgender" >男</button> 　<button class="selectgender" >女</button> 　<button class="selectgender" >保密</button> 　　
-                                
+                                性别： <button class="selectgender" @click="setmale" id="male">男</button> 　<button @click="setfemale" class="selectgender" id="female">女</button>　　                       
                             </div>
                             <div  class="changesubInfo">
                                 生日： <input type="date" id="changebirth" class="change" v-model="userbirth"/>
@@ -17,11 +20,12 @@
                         <span >
                         <hr class="cutline2">
                         </span>
-                          <button id="submit" >保存</button>
+                          <button id="submit" @click="changeinfo">保存</button>
                         </div>
 </template>
 
 <script>
+import https from "@/https.js";
     export default {
         name: "changeinfo",
         data() {
@@ -33,12 +37,55 @@
                 userloc:"江苏",
                 usergender:"男",
                 usercol:"牛津大学",
-                useremail:"631570777@qq.com",
+                useremail:"",
                 userphone:10086,
-                userbirth:"1840年9月7日",
+                userbirth: '',
                 cur: 0 //默认选中第一个tab
             }
+
+        },
+        mounted() {
+            this.getUserInfo()
+        },
+         methods: {
+             setmale(){
+                 this.usergender='male'
+             },
+              setfemale(){
+                 this.usergender='female'
+             },
+              getUserInfo() {
+                let userinfo = JSON.parse(localStorage.getItem('userinfo'))
+                console.log(userinfo)
+                this.username = userinfo.userName
+                this.usergender = userinfo.userGender 
+                this.userbirth = userinfo.userBirthday
+                this.usersign=userinfo.userSignature
+                this.useremail = userinfo.userEmail
+                this.userphone = userinfo.userPhoneNumber
+            },
+            changeinfo() {
+                console.log(this.userbirth)
+                let params = {userName: this.username, userGender: this.usergender,userBirthday:this.userbirth,userSignature:this.usersign};
+                https.fetchPost('/user/userModify', params)
+                    .then(data => {
+                        if(data.data.code==200){
+                            let userinfo=data.data.data
+                            console.log(userinfo)
+                            localStorage.setItem('userinfo', JSON.stringify(userinfo))
+                            this.$router.push('/userinfo/basicinfo')
+                        }
+                        else{
+                            alert('错误码:'+data.data.data)
+                        }
+                    }).catch(err =>{
+                        alert(err.toString())
+                })
+            }
+
         }
+        
+        
     }
    
 </script>
